648434ebbb37f55cec6cf27fd704b4597b95268d
Simon <le6monstre@hotmail.com>
2018-08-21 10:48:06 -0400

    Configured the project.

---
+++ Assets/Scripts/Playmode/Bird/BirdPhysics.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class BirdPhysics : MonoBehaviour
+    {
+        [SerializeField] private float flapForce = 1;
+
+        private new Rigidbody2D rigidbody;
+
+        private void Awake()
+        {
+            rigidbody = transform.root.gameObject.AddComponent<Rigidbody2D>();
+        }
+
+        public void Flap()
+        {
+            rigidbody.AddForce(Vector2.up * flapForce, ForceMode2D.Impulse);
+        }
+    }
+}
